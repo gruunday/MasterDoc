774cf83152f29debe6bbcfa6cb0102046b5c9a2d
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-05-28 09:19:55 +0100

    Fixed config bugs

--- src/collector.py
+++ src/collector.py
@@ -13,8 +13,6 @@ def get_docs_urls(repo_lst):
     docs_urllst = []
     # Goes through every repo in organisation
     for repo in repo_lst:
-        if repo.name == 'docs':
-            continue
         contents = repo.get_contents("")
         # For every root file/directory
         for content_file in contents:
@@ -35,7 +33,7 @@ def create_file_struct(url_lst):
         url = url.split('/')
         repo = url[4]
         folder = ''.join(url[7:-1])
-        path = f'../docs/{repo}/{folder}'
+        path = f'docs/{repo}/{folder}'
         if not os.path.isdir(path):
             os.makedirs(path)
 
@@ -46,11 +44,11 @@ def download_files(url_lst):
         repo = surl[4]
         folder = ''.join(surl[7:-1])
         name = surl[-1]
-        filename = f'../docs/{repo}/{folder}/{name}'
+        filename = f'docs/{repo}/{folder}/{name}'
         if repo not in contents_dict:
             contents_dict[repo] = [name]
         else:
-            contents_dict[repo].append(name)
+            contents_dict[repo].append(f'{folder}/{name}')
         urllib.request.urlretrieve(url, filename)
     return contents_dict
 
@@ -68,16 +66,14 @@ extra_javascript:
   - js/graphviz.js
 extra_css:
   - css/custom.css
-pages:
+nav:
   - Home: index.md\n"""
     with open('mkdocs.yml', 'w') as f:
         f.write(header)
         for repo in contents:
-            if repo == 'RFCs':
-                continue
             f.write(f'  - {repo}:\n')
             for page in contents[repo]:
-                f.write(f'      - {page}: {repo}/{page}\n')
+                f.write(f'      - {page.split("/")[-1]}: {repo}/{page}\n')
 
     
 def usage():
