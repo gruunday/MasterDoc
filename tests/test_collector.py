9570a6f5c9a7392e18f3582ce31f8aaaee4a8583
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-05-29 10:08:57 +0100

    Added first test

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -1,1 +1,14 @@
-import collector
+from src import collector as col, config
+import unittest
+from github import Github
+
+
+
+class TestImgManip(unittest.TestCase):
+
+    def setUp(self):
+        self.g = Github(config.github_secret)
+    
+    def test_ls(self):
+        self.repo_lst = col.lst_repos(self.g)
+        self.assertEqual(type(self.repo_lst),type([]))
